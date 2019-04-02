137f833f2781fc72efa3f6881597018b838615c6
HoudaDabou <43133237+HoudaDabou@users.noreply.github.com>
2019-04-03 00:20:59 +0200

    Update protractor.conf.js

--- e2e-tests/protractor.conf.js
+++ e2e-tests/protractor.conf.js
@@ -14,7 +14,7 @@ exports.config = {
   chromeOnly: true,
   directConnect: true,
 
-  baseUrl: 'http://localhost:8000/',
+  baseUrl: 'http://localhost:8080/',
 
   framework: 'jasmine',
   jasmineNodeOpts: {
